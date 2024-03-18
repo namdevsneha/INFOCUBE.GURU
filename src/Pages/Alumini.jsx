adc4d20bb3a1de56603758f4536900d0912740e6
noddy0070 <utkarshsaxena@rediffmail.com>
2024-03-18 19:02:40 +0530

    adding routes

--- src/Pages/Alumini.jsx
+++ src/Pages/Alumini.jsx
@@ -1,9 +1,7 @@
-import React from "react";
+import React from "react"
 
 export default function Alumini(){
     return (
-        <div>
-        hello
-        </div>
+        <div> Alumin </div>
     )
 } 
