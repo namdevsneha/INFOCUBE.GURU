ebe1606fc90fefdae23cbd44c4a64bbc455bd967
namdevsneha <snehanamdev40@gmail.com>
2024-03-19 16:03:06 +0530

    sneha/join

---
+++ client/src/Components/Home/Join.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Join() {
+  return (
+    <div>
+      <h2 className='font-roboto text-base '>Career Counselling And Coaching</h2>
+      <h1 className='font-roboto text-5xl font-bold '>Unlock Your Potential with Personalized Guidance</h1>
+      <p>At our platform, we provide you with narrowed road maps and detailed career paths to help you navigate your chosen field. Our community of alumni and mentors are always available to offer personalized guidance and support. Stay updated with the latest news and reminders in your field of interest.</p>
+    </div>
+  )
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors:{
+      buttonColor:'#255652',
+    },
     extend: {
       fontFamily: {
         roboto: ['Roboto', 'sans-serif'],
