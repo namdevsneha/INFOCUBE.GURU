6d8b639397658935cb2be50b46836088e655dd2b
namdevsneha <snehanamdev40@gmail.com>
2024-03-20 00:27:26 +0530

    updating components

---
+++ client/src/Components/Home/Check.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+export default function Check(){
+    return (
+        <div className="w-full relative overflow-hidden flex flex-col items-center justify-center py-[3.75rem] px-[4rem] box-border text-left text-[3rem] text-black font-roboto">
+        <div className="flex flex-row items-center justify-start gap-[5rem]">
+        <img className="w-[38.5rem] relative h-[40rem] overflow-hidden shrink-0" alt="" src="Placeholder / Lightbox.svg" />
+        <div className="w-[38.5rem] flex flex-col items-start justify-center gap-[1.5rem]">
+        <div className="self-stretch flex flex-col items-start justify-start">
+        <div className="w-[5rem] relative h-[5rem] overflow-hidden shrink-0" />
+        <div className="self-stretch flex flex-col items-start justify-start gap-[1.5rem]">
+        <b className="self-stretch relative leading-[120%]">Stay Updated with the Latest News and Reminders in Your Field</b>
+        <div className="self-stretch relative text-[1.125rem] leading-[150%]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</div>
+        </div>
+        </div>
+        <div className="flex flex-row items-center justify-start pt-[1rem] px-[0rem] pb-[0rem] text-[1rem] text-white">
+        <div className="rounded-[44px] bg-darkslategray flex flex-row items-center justify-center py-[0.75rem] px-[1.5rem]">
+        <div className="relative leading-[150%]">CHECK OUT</div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>)
+
+}
---
+++ client/src/Components/Home/Image.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function ImageComponent(){
+    return (
+
+<div className="w-full relative overflow-hidden flex flex-col items-center justify-start py-[3.75rem] px-[4rem] box-border gap-[2.813rem] text-center text-[3rem] text-black font-roboto">
+<div className="w-[48rem] flex flex-col items-center justify-start gap-[1.5rem]">
+<b className="self-stretch relative leading-[120%]">Image Gallery</b>
+<div className="self-stretch relative text-[1.125rem] leading-[150%]">Capturing Moments of Community Events and Interactions</div>
+</div>
+<div className="self-stretch flex flex-row items-start justify-start gap-[2rem]">
+<div className="flex flex-col items-start justify-start gap-[2rem]">
+<img className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full object-cover" alt="" src="Placeholder / Image.png" />
+<img className="w-[40rem] relative h-[26.625rem] object-cover" alt="" src="Placeholder / Image.png" />
+</div>
+<div className="flex-1 flex flex-col items-start justify-start gap-[2rem]">
+<img className="w-[40rem] relative h-[26.625rem] object-cover" alt="" src="Placeholder / Image.png" />
+<img className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full object-cover" alt="" src="Placeholder / Image.png" />
+</div>
+</div>
+</div>)
+}
