d36217819ff22a34a5e6dfc793854291cd710e99
namdevsneha <snehanamdev40@gmail.com>
2024-03-22 12:29:11 +0530

    sneha/journey

--- client/src/Components/Home/Journey.jsx
+++ client/src/Components/Home/Journey.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import bg from '../../Assets/Images/bg.svg';
 
 export default function Journey(){
     return (
-        <div className="w-full relative flex flex-col items-center justify-start gap-[1.5rem] text-center text-[3rem] text-white font-roboto">
-<div className="self-stretch flex flex-col items-center justify-start gap-[1.5rem]">
-<b className="self-stretch relative leading-[120%]">Start Your Journey</b>
-<div className="self-stretch relative text-[1.125rem] leading-[150%]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</div>
-</div>
-<div className="flex flex-row items-start justify-start pt-[1rem] px-[0rem] pb-[0rem] gap-[1rem] text-left text-[1rem]">
-<div className="rounded-23xl bg-darkslategray flex flex-row items-center justify-center py-[0.75rem] px-[1.5rem] border-[1px] border-solid border-darkslategray">
-<div className="relative leading-[150%]">Sign Up</div>
-</div>
-<div className="rounded-23xl flex flex-row items-center justify-center py-[0.75rem] px-[1.5rem] border-[1px] border-solid border-white">
-<div className="relative leading-[150%]">{`Log in `}</div>
-</div>
-</div>
-</div>
+        <div className='relative'>    
+                <img  src={bg}/>
+
+        <div className="absolute left-0 top-0 w-full h-full flex flex-col items-center justify-center text-[3rem] text-center text-black font-roboto bg-local">
+
+        <div className="self-stretch flex flex-col items-center justify-start gap-[1.5rem]">
+    
+        <h1 className="self-stretch relative leading-[120%]">Start Your Journey</h1>
+
+        <div className="self-stretch relative text-[1.125rem] leading-[150%]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</div>
+        </div>
+
+        <div className="flex flex-row items-start justify-start pt-[1rem] px-[0rem] pb-[0rem] gap-[1rem] text-left text-[1rem]">
+
+        <div className="rounded-23xl bg-darkslategray flex flex-row items-center justify-center py-[0.75rem] px-[1.5rem] border-[1px] border-solid border-darkslategray">
+        <div className="relative leading-[150%] text-white">Sign Up</div>
+        </div>
+        <div className="rounded-23xl flex flex-row items-center justify-center py-[0.75rem] px-[1.5rem] border-[1px] border-solid border-black text-black">
+        <div className="relative leading-[150%]">Login</div>
+        </div>
+        </div>
+
+        </div>
+
+        </div>
     )
 } 
