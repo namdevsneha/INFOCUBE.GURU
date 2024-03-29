9608b1fc1204e5f9c634099b593098585a2067c9
noddy0070 <utkarshsaxena@rediffmail.com>
2024-03-30 01:24:57 +0530

    adding mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connection to mongodb success.")
+}).catch((err)=>{
+    console.log(err)
+});
 
 const app=express();
+
 
 app.listen(3000,()=>{
     console.log("Hello server is running.");
