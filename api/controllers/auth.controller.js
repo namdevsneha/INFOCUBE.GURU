0320797e52179105ade74fbce96e5bf7686cf751
noddy0070 <utkarshsaxena@rediffmail.com>
2024-05-17 00:20:48 +0530

    form after google login

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,9 +5,9 @@ import jwt from "jsonwebtoken";
 
 
 export const signup= async(req,res,next)=>{
-   const{username,email,password}= req.body;
+   const{username,email,password,education,dob,gender}= req.body;
    const hashedPassword=bcryptjs.hashSync(password,10);
-   const newUser= new User({username,email,password:hashedPassword});
+   const newUser= new User({username,email,password:hashedPassword,education,dob,gender});
    try{
     await newUser.save();
     res.status(201).json("user created");
@@ -15,6 +15,28 @@ export const signup= async(req,res,next)=>{
      next(error)
    }
    
+}
+
+export const signup2=async(req,res,next)=>{
+
+  const{username,email,education,dob,gender}= req.body;  
+  try{
+    const generatePassword=Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+    const hashedPassword=bcryptjs.hashSync(generatePassword,10);
+    const newUser=new User({username:username,
+    email:email,password:hashedPassword,verified:true,education:education,dob:dob,gender:gender})
+    await newUser.save();
+    const token= jwt.sign({id:newUser._id},process.env.JWT_SECRET);
+    const {verified:veri,password:pass,...rest}=newUser._doc;
+
+    res
+    .cookie('access_token',token,{httpOnly:true})
+    .status(200)
+      .json({email:req.body.email});
+
+  }catch(error){
+    next(error)
+  }
 }
 
 export const login =async(req,res,next)=>{
@@ -45,18 +67,19 @@ export const google=async(req,res,next)=>{
       .status(200)
       .json(rest);
     }else{
-      const generatePassword=Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
-    const hashedPassword=bcryptjs.hashSync(generatePassword,10);
-    const verify=true;
-    const newUser=new User({username:req.body.name,
-    email:req.body.email,password:hashedPassword,avatar:req.body.photo,verified:true})
-    await newUser.save();
-     const token= jwt.sign({id:newUser._id},process.env.JWT_SECRET);
-      const {verified:veri,password:pass,...rest}=newUser._doc;
-      res
-      .cookie('access_token',token,{httpOnly:true})
-      .status(200)
-      .json(rest);  
+      // const generatePassword=Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+      // const hashedPassword=bcryptjs.hashSync(generatePassword,10);
+      // const verify=true;
+      // const newUser=new User({username:req.body.name,
+      // email:req.body.email,password:hashedPassword,avatar:req.body.photo,verified:true})
+      // await newUser.save();
+      // const token= jwt.sign({id:newUser._id},process.env.JWT_SECRET);
+      // const {verified:veri,password:pass,...rest}=newUser._doc;
+
+      res.status(200)
+      .json({email:req.body.email});
+      // .cookie('access_token',token,{httpOnly:true})
+        
     }
   } catch (error) {
     console.log(error);
