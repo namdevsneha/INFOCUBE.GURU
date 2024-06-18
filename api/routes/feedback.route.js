4506db59beab05f1882024204b29f950fddef5ea
noddy0070 <utkarshsaxena@rediffmail.com>
2024-06-19 01:39:31 +0530

    feedback fetch

--- api/routes/feedback.route.js
+++ api/routes/feedback.route.js
@@ -1,7 +1,9 @@
 import  express from "express";
-import { feedback, feedbackFetch } from "../controllers/feedback.controller.js";
+import { feedback, feedbackFetch, fetchUserData } from "../controllers/feedback.controller.js";
 const router = express.Router();
 
 router.post("/save",feedback)
 router.post("/fetch",feedbackFetch)
+router.post("/fetchUserData",fetchUserData)
+
 export default router;
