e4e5e12ead786bf236249c02ff1a1a9875b272f6
noddy0070 <utkarshsaxena@rediffmail.com>
2024-06-25 19:42:58 +0530

    chore: Update lazy loading for images in Join, Expertise, Check, and Image components

--- api/index.js
+++ api/index.js
@@ -18,8 +18,11 @@ mongoose.connect(process.env.MONGO).then(()=>{
 const __dirname=path.resolve();
 
 const app=express();
+
+
 app.use(express.json())
 app.use(cookieParser());
+
 
 app.listen(3000,()=>{
     console.log("Hello server is running.");
