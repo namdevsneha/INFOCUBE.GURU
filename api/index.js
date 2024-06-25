f66a07538e7ce4da009217287e7d61eb8dcd3969
noddy0070 <utkarshsaxena@rediffmail.com>
2024-06-26 00:31:00 +0530

    chore: Update API route for homepage and remove unnecessary code

--- api/index.js
+++ api/index.js
@@ -39,9 +39,9 @@ app.use('/api/feedback',feedbackRouter);
 
 app.use(express.static(path.join(__dirname,'/client/dist')));
 
-app.get('/',(req,res)=>{
+app.get('*',(req,res)=>{
     // res.sendFile(path.join(__dirname,'client','dist','index.html'));
-    console.log("Hello");
+    console.log("hello");
 })
 
 
