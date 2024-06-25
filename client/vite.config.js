4e4bf5d2279bcaed1fb97980681738c52bba5b6a
noddy0070 <utkarshsaxena@rediffmail.com>
2024-06-26 02:00:02 +0530

    chore: Update API route for homepage to use secure HTTPS connection and proxy requests to https://infocube-guru-api.vercel.app

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
-    '/api': {
+    proxy:{
+      '/api': {
         target: 'https://infocube-guru-api.vercel.app',
         secure: true,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
+    }
   },
   plugins: [react()],
 })
